8dc3846c0303fdc1b86a395f7a74d8717c883322
Kenny Yang <kennyyang19@gmail.com>
2018-08-04 22:50:54 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+  "My name is Kenny"
+  end
+
+  get '/hometown' do
+    "My hometown is Kenny"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Friday"
+  end
+
 end
