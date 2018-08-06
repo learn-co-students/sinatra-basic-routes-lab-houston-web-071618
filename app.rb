9d0246da4c027fe28f913bc20a958aa4cd1008ed
Ryhan <ryhan.chowdhury3@gmail.com>
2018-08-05 21:04:48 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Ryhan"
+  end
+
+  get '/hometown' do
+    "My hometown is Houston"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Poop"
+  end
 end
